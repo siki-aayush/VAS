8cf3986ea9990f7d3ee450a28856a2bcdd5e23be
siki <musyajuaayush@gmail.com>
2022-07-26 20:28:01 +0545

    Updates the login UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/login/Login';
+
+import 'antd/dist/antd.min.css';
+import './App.css';
+import './global.css';
 
 function App() {
   return (
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -10,27 +10,47 @@ const Login: React.FC = () => {
     console.log('failed');
   };
   return (
-    <Form
-      name="login"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 18 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      className="login"
-    >
-      <Form.Item name="username" label="Username" rules={[{ required: true, message: 'Please input your username!' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="password" label="Password" rules={[{ required: true, message: 'Please input your password!' }]}>
-        <Input.Password />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login__submit">
-          Log in
-        </Button>
-      </Form.Item>
-    </Form>
+    <div className="wrapper">
+      <div className="card">
+        <h1 className="card__title">
+          Welcome to <span>VAS</span>
+        </h1>
+        <Form
+          layout="vertical"
+          size="large"
+          name="login"
+          // labelCol={{ span: 8 }}
+          // wrapperCol={{ span: 18 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          className="login"
+        >
+          <Form.Item
+            name="username"
+            label="Username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input className="login__username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password className="login__password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login__submit">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+        <div className="card__footer">
+          Don't have an account? <span>Sign up for free</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
